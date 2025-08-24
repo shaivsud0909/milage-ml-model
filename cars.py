e3be9b7556f6b78491f6fcb4bfae20f0ffa68b5e
Shaiv Sud <shaivsood@gmail.com>
2025-08-24 22:42:37 +0530

    one hot label

--- cars.py
+++ cars.py
@@ -6,14 +6,21 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import numpy as np
 
 
-# Load dataset
 data = pd.read_csv("cars.csv")
 
+categorical_cols = ["Make", "Fuel_Type", "Transmission", "Drivetrain"]
+numerical_cols = ["Weight_kg", "Engine_cc", "Wheel_Diameter_in", "Power_hp", "Cylinders"]
 
-x = data[["Weight_kg","Engine_cc","Wheel_Diameter_in","Power_hp","Cylinders"]]
+
+data_encoded = pd.get_dummies(data[categorical_cols], drop_first=True)  # drop first to avoid dummy variable trap
+
+
+x = pd.concat([data[numerical_cols], data_encoded], axis=1) #axis 1 because horizontal concatenation
+
+
 y = data["Mileage_kmpl"]
 
-# Split 
+
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
 
@@ -29,16 +36,35 @@ print("R2 Score:", r2_score(y_test, y_pred))
 
 
 name = input("Enter the name of the car: ")
+make = input("Enter the make of the car: ")
+fuel = input("Enter fuel type (Petrol/Diesel): ")
+trans = input("Enter transmission (Manual/Automatic/DCT/CVT): ")
+drive = input("Enter drivetrain (FWD/RWD/4WD): ")
 weight_kg = float(input("Enter the weight in kg: "))
 engine_cc = float(input("Enter engine size in cc: "))
 wheel_dia = float(input("Enter wheel diameter (inches): "))
 power_hp = float(input("Enter power in HP: "))
 cylinders = int(input("Enter number of cylinders: "))
 
+# Prepare input
+input_data = pd.DataFrame([{
+    "Weight_kg": weight_kg,
+    "Engine_cc": engine_cc,
+    "Wheel_Diameter_in": wheel_dia,
+    "Power_hp": power_hp,
+    "Cylinders": cylinders,
+    "Make": make,
+    "Fuel_Type": fuel,
+    "Transmission": trans,
+    "Drivetrain": drive
+}])
 
-input_data = pd.DataFrame([[weight_kg, engine_cc, wheel_dia, power_hp, cylinders]],
-                          columns=["Weight_kg","Engine_cc","Wheel_Diameter_in","Power_hp","Cylinders"])
+# Encode 
+input_encoded = pd.get_dummies(input_data, columns=categorical_cols)
 
+# Align 
+input_encoded = input_encoded.reindex(columns=x.columns, fill_value=0)
 
-predicted_mileage = model.predict(input_data)
-print(f"The predicted mileage for {name} is {predicted_mileage[0]:.2f} kmpl")
+# Predict
+predicted_mileage = model.predict(input_encoded)
+print(f"\nThe predicted mileage for {name} is {predicted_mileage[0]:.2f} kmpl")
